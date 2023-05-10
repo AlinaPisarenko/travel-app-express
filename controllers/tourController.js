8a11114618414307974fbf54632a4702bbdaa14f
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-05-10 17:56:04 -0400

    updated schema, imported data to DB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -19,13 +19,29 @@ const Tour = require('./../models/tourModel')
 exports.getAllTours = async (req,res) => {
     
     try {
-        const tours = await Tour.find()
+        
+        //BUILD QUERY
+        const queryObj = {...req.query}
+        const excludedFields = ['page', 'sort', 'limit', 'fields']
+        excludedFields.forEach(el => delete queryObj[el])
 
-    res.status(200).json({
-        status: 'success',
-        result: tours.length,
-        data: {
-            tours
+        const query = Tour.find(queryObj)
+
+        // const query = Tour.find()
+        //     .where('duration')
+        //     .equals(5)
+        //     .where('difficulty')
+        //     .equals('easy')
+
+        // EXECUTE QUERY
+        const tours = await query
+
+        //SEND RESPONSE
+        res.status(200).json({
+            status: 'success',
+            result: tours.length,
+            data: {
+                tours
         }
     })
     } catch (err) {
