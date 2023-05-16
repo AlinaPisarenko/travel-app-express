4df8b214520949cea78b9897d26655e634b8b41c
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-05-16 11:57:08 -0400

    implemented validators

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -92,7 +92,7 @@ exports.createTour = async (req,res) => {
     } catch (err) {
         res.status(400).json({
         status: 'fail',
-        message: "Invalid data sent 🥲"
+        message: err
     }); 
     }
 
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify')
+const validator = require('validator')
+
 
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a name'],
         unique: true,
-        trim: true
+        trim: true,
+        maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+        minlength: [10, 'A tour name must have more or equal than 10 characters'],
+        // validate: [validator.isAlpha, 'Tour name must only contain characters.']
     },
     slug: {
         type: String
@@ -21,7 +26,11 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour must have a difficulty']
+        required: [true, 'A tour must have a difficulty'],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: ' Difficulty is either: easy, medium, difficult'
+        } 
     },
     ratingsQuantity: {
         type: Number,
@@ -29,13 +38,25 @@ const tourSchema = new mongoose.Schema({
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be below 5.0']
     },
     price: {
         type: Number,
         required: [true, 'A tour must have a price']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+        type: Number,
+        validate: {
+            validator: function(val) {
+            // this only points to current doc on NEW document creation
+            return val < this.price
+        },
+            message: 'Discount price ({VALUE}) should be below the regular price'
+        }
+        
+    },
     summary: {
         type: String,
         trim: true,
@@ -98,6 +119,13 @@ tourSchema.post(/^find/, function(docs, next) {
     // tourSchema.pre('find', function(next) {
     // console.log(docs)
     console.log(`Query took, ${Date.now() - this.start} milliseconds⏰`)
+    next()
+})
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function(next) {
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true }} })
+    // console.log(this.pipeline())
     next()
 })
 
