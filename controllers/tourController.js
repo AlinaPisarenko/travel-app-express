07c8ad2c69ff90134cb079f677c78678e2d0c80e
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-05-22 16:39:04 -0400

    implemented geospatial data. Populated Tour Guides

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -36,7 +36,9 @@ exports.getAllTours = catchAsync(async (req,res, next) => {
 // GET ONE TOUR
 exports.getTour = catchAsync(async (req,res, next) => {
  
-        const tour = await Tour.findById(req.params.id)
+        const tour = await Tour
+            .findById(req.params.id)
+            
         // const tour = await Tour.findOne({ _id: req.params.id })
         if (!tour) {
             return next(new AppError('No tour found with that ID', 404))
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify')
-const validator = require('validator')
+// const User = require('./userModel')
+
 
 
 const tourSchema = new mongoose.Schema({
@@ -79,7 +80,37 @@ const tourSchema = new mongoose.Schema({
     secretTour: {
         type: Boolean, 
         default: false
-    }
+    },
+    startLocation: {
+        //GeoJSON
+        type: {
+            type: String,
+            default: 'Point',
+            enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String
+    },
+    locations: [
+        {
+            type: {
+                type: String,
+                default: 'Point',
+                enum: ['Point']
+            },
+            coordinates: [Number],
+            address: String,
+            description: String,
+            day: Number
+        },  
+    ],
+    guides: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User'
+        }
+    ]
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
@@ -96,6 +127,12 @@ tourSchema.pre('save', function(next) {
     next()
 })
 
+// tourSchema.pre('save', async function(next) {
+//     const guidesPromises = this.guides.map(async id => await User.findById(id))
+//     this.guides = await Promise.all(guidesPromises)
+//     next()
+// })
+
 
 // QUERY MIDDLEWARE
 tourSchema.pre(/^find/, function(next) {
@@ -105,12 +142,22 @@ tourSchema.pre(/^find/, function(next) {
     next()
 })
 
+tourSchema.pre(/^find/, function(next) {
+    this.populate({
+            path: 'guides',
+            select: '-__v -passwordChangedAt'
+            })
+    next()
+})
+
 tourSchema.post(/^find/, function(docs, next) {
     // tourSchema.pre('find', function(next) {
     // console.log(docs)
     console.log(`Query took, ${Date.now() - this.start} milliseconds⏰`)
     next()
 })
+
+
 
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function(next) {
