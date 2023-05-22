e94b5874dc8a03805b89d2fc09a97d3b6b60e12a
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-05-22 13:48:26 -0400

    implemented limit on amount of requests, set security HTTP headers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -9,17 +11,36 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// MIDDLEWARE
+
+// GLOBAL MIDDLEWARE
+
+
+// Set security HTTP headers
+app.use(helmet())
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
-app.use(express.json());
+// Limit amount of request per one IP address
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60* 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour'
+})
+
+app.use('/api', limiter)
+
+// Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static foles
 app.use(express.static(`${__dirname}/public`))
 
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString()
-
     next()
 })
 
@@ -35,8 +56,3 @@ app.use(globalErrorHandler)
 
 module.exports = app
 
-// app.get('/api/v1/tours', getAllTours)
-// app.post('/api/v1/tours', createTour)
-// app.get('/api/v1/tours/:id', getTour)
-// app.patch('/api/v1/tours/:id', updateTour)
-// app.delete('/api/v1/tours/:id', deleteTour)
