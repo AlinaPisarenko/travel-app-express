7f97d6a82f6d7908a9744913b2e7e97c35b943b5
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-08-01 14:33:52 -0400

    test

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12459,7 +12459,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54028" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52727" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
