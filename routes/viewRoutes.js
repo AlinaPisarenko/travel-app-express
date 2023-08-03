295dabaf135233337b20a16d82de0fbf8cddda53
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-08-03 14:23:10 -0400

    fixed bug in creating a booking

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { getOverview, getTour, getLoginForm, getSignupForm, getAccount, updateUserData } = require('../controllers/viewsController')
 const authController = require('../controllers/authController')
+const bookingController = require('../controllers/bookingController')
+
 
 const router = express.Router()
 
-router.get('/', authController.isLoggedIn, getOverview)
+router.get('/',bookingController.createBookingCheckout, authController.isLoggedIn, getOverview)
 router.get('/tour/:slug', authController.isLoggedIn, getTour)
 router.get('/login',authController.isLoggedIn, getLoginForm)
 router.get('/signup',authController.isLoggedIn, getSignupForm)
